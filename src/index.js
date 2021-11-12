6d3224a6065f165476f5e90cceeddd3b822da8d5
RohitTaparia <rohitktaparia@gmail.com>
2021-11-13 00:30:03 +0530

    Revert "finished"
    This reverts commit e2b707a19e7b76bc94ade69e4b4f26ae051ee2a9.

--- src/index.js
+++ src/index.js
@@ -2,40 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/*
+Square is just a function component
+When clicked it puts the appropriate design from css file
+You need to just assign the correct div name for each value
+check css file for div names, the ones needed here are
+circle red-player, circle yellow-player, circle
+I have put dollar signs where you need to fill up
+*/
+
 function Square(props) {
     return (
       <button className="square" onClick={() => props.onClick()}>
-        <div className={props.value === 'Red' ? "circle red-player" : props.value === 'Yellow' ? "circle yellow-player" : "circle"}></div>
+        <div className={props.value === 'Red' ? $$$ : props.value === 'Yellow' ? $$$ : $$$}></div>
       </button>
     );
 }
 
+/*
+in renderSquare call the square component
+pass value and onclick props to it
+
+in the render method of Col component
+call render square for all squares in
+1 column that is from 0-5.
+inside the div board-col
+*/
 class Col extends React.Component {
 
     renderSquare(i) {
-      return (
-        <Square
-          value={this.props.colValues[i]}
-          onClick={() => this.props.colOnClick()}
-        />
-      );
+      //Call Square here
     }
   
     render() {
       return (
         <div className="board-col">
-          {this.renderSquare(5)}
-          {this.renderSquare(4)}
-          {this.renderSquare(3)}
-          {this.renderSquare(2)}
-          {this.renderSquare(1)}
-          {this.renderSquare(0)}
+          
         </div>
       );
     }
-  }
+}
 
 class Board extends React.Component {
+    //Default constructor values
+    //NOTE: our board array is column x row format 
+    //hence 7 x 6, don't get confused
     constructor() {
         super();
         this.state = {
@@ -45,242 +56,133 @@ class Board extends React.Component {
         }
     }
 
-    filled(board) {
-        for(let c = 0; c < 7; c++) {
-            if(board[c][5] === null) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    miniMax(tempBoard, curPlayer, depth, alpha, beta, track) {
+    miniMax(tempBoard, curPlayer, depth, track) {
         let score;
-        if(depth === 0 || this.filled(tempBoard) || gameOver(tempBoard)) {
-            score = get_state(tempBoard);
-            return score;
-        }
+        //check if terminal state, depth == 0 or gameover or board filled(DRAW)
+        //call get_state and return it
 
         const opponent = (curPlayer === 'Red')? 'Yellow' : 'Red';
-        var best = (curPlayer === 'Red')? 50000 : -50000;
-        var pos = 3;
+        var best = (curPlayer === 'Red')? 50000 : -50000; // random big values i took
+        var pos = 3; // default can be anything doesn't matter
 
-        for(let c = 0; c < 7; c++) {
-            if(track[c] < 6) {
-                //Maximizer
-                if(curPlayer === 'Yellow') { 
-                    tempBoard[c][track[c]] = curPlayer;
-                    track[c] += 1;
-                    score = this.miniMax(tempBoard, opponent, depth-1, alpha, beta, track);
-                    //score = score*depth;
+        /* loop for all columns from 0-6
+        if yellow(computer turn) then use maximizer function
+        else if red(player turn) use minimzer function
+        keep updating tempBoard & track when you pass them for recursion, decrease depth also by 1
+        and once it comes back revert the change back to normal to pass for next iteration
+        best should store the highest or lowest score possible depending on who current player is
+        */
 
-                    if(score > best) {
-                        best = score;
-                        pos = c;
-                        // update alpha
-                        if(best > alpha) {
-                            alpha = best;
-                        }
-
-                        if(beta <= alpha) {
-                            track[c] -= 1;
-                            tempBoard[c][track[c]] = null;
-                            break;
-                        }
-                    }
-                    track[c] -= 1;
-                    tempBoard[c][track[c]] = null;
-                }
-                else { //Minimizer
-                    tempBoard[c][track[c]] = curPlayer;
-                    track[c] += 1;
-                    score = this.miniMax(tempBoard, opponent, depth-1, alpha, beta, track);
-                    //score = score*depth;
-
-                    if(score < best) {
-                        best = score;
-                        pos = c;
-                        // update beta
-                        if(best < beta) {
-                            beta = best;
-                        }
-
-                        if(beta <= alpha) {
-                            track[c] -= 1;
-                            tempBoard[c][track[c]] = null;
-                            break;
-                        }
-                    }
-                    track[c] -= 1;
-                    tempBoard[c][track[c]] = null;
-                }
-
-            }
-        } //loop ends
-
-        if(depth === 8) {
-            return pos;
-        }
-        else {
-            return best;
-        }
+        /* if this was the starting point of recursion check depth == whatever value you set 
+        in AIplay then return the position to play
+        else return best which stored the best score we could get from this path */
+        
     }
 
     AIplay() {
-        if(this.state.winner != null || this.filled(this.state.boardValue))
-            return;
+        //check if winner exists and return
         
         const val = this.state.boardValue.slice();
         const curPlayer = this.state.redIsNext? 'Red' : 'Yellow';
         
-        var depth = 8;
+        var depth = 6 //try changing this value in your browsers
+        //in mine this works at a decent speed
         var track = new Array(7).fill(7);
-
         //Track first empty position of each column
-        for(let i = 0; i < 7; i++) {
-            for(let j = 0; j < 6; j++) {
-                if(val[i][j] === null) {
-                    track[i] = j;
-                    break;
-                }
-            }
-        }
+        //track[i] = the position in the ith column where the next turn will fall
 
-        var pos = this.miniMax(val, curPlayer, depth, -50000, 50000, track);
+        var pos = this.miniMax(val, curPlayer, depth, track);
+        //returns which column is the most optimal to play in
 
-        let findResult;
-        for(let j = 0; j < 6; j++) {
-            if(val[pos][j] === null) {
-                val[pos][j] = curPlayer;
-                findResult = gameOver(val);
-                break;
-            }
-        }
+        //put the computer token in pos column
+        //check if game over
 
-        var curWinner = findResult? curPlayer : null;
-        this.setState({
-            boardValue: val,
-            redIsNext: !this.state.redIsNext,
-            winner: curWinner
-        });
+        //set state again like how in player
+        //no callback function needed
     }
-    
+
     handleClick(col) {
-        if(this.state.winner != null || this.filled(this.state.boardValue))
-            return;
+        /* Check if there exists a winner 
+        If winner is not null then return since game is over 
+        and any other click should not continue playing*/
 
         const val = this.state.boardValue.slice();
         const curPlayer = this.state.redIsNext? 'Red' : 'Yellow';
         let findResult;
 
-        for(let i = 0; i < 6; i++) {
-            if(val[col][i] === null) {
-                val[col][i] = curPlayer;
-                findResult = gameOver(val);
-                break; 
-            }
+        /*val is our current board
+        check for the [col][0-5] which is the first free cell
+        since in connect4 there can only be 1 position 
+        for each column in a particular turn
+        assign that cell the curPlayer value
+        after making the turn call gameOver function
+        and assign that value to findResult
+        NOTE: when looping from 0-5 if we have reached the end(5)
+        of the loop and there is no empty cell that implies the column
+        is filled, so return immediately since the play is not possible
+        NEED to use some jump statements like return, break, goto, continue etc...*/
 
-            // reached topmost square
-            if(i === 5) {
-                return;
-            }
-        }
 
+        /*THE BELOW LINE CHECKS IF GAME IS OVER NOW
+        if it is we know that the curPlayer has won since 
+        he is the one who made the move*/
         var curWinner = findResult? curPlayer : null;
+
+        /*call this.setState now
+        boardValue will be val
+        redIsNext should be complemented
+        winner assign curWinner*/
         this.setState({
             boardValue: val,
             redIsNext: !this.state.redIsNext,
             winner: curWinner,},() => {
-            this.AIplay();
+            this.AIplay(); // Call to function for computer to take it's turn
         });
     }
 
     renderCol(i) {
-        return (
-        <Col
-            colValues={this.state.boardValue[i]}
-            colOnClick={() => this.handleClick(i)}
-        />
-        );
-    }
-
-    handleReset()
-    {   const val = this.state.boardValue.slice();
-        for(let c = 0;c<7;c++)
-        {
-            for(let r = 0;r<6;r++)
-            {
-                val[c][r] = null;
-            }
-        }
-        this.setState({boardValue: val,
-            redIsNext : true,
-            winner : null });
+        /* Call Column Component Here
+        Pass the onclick handler and
+        that column's values */
     }
 
     render() {
         var status;
-        if(this.state.winner) {
-            status = "Winner: " + (this.state.winner);
-        }
-        else if(this.filled(this.state.boardValue)) {
-            status = 'Game Drawn';
-        }
-        else {
-            status = 'Next Player: ' + (this.state.redIsNext? 'Red' : 'Yellow');
-        }
+        // If there exists a winner status should show winner is ---
+        // Else status should show next player is ---
+        // Use the state values winner and isRedNext for doing the above conditions easily
 
+        /* Render all 7 columns (0-6)
+        inside the div game-board */
         return (
             <div className="game">
                 <div className="status">{status}</div>
                 <div className="game-board">
-                {this.renderCol(0)}
-                {this.renderCol(1)}
-                {this.renderCol(2)}
-                {this.renderCol(3)}
-                {this.renderCol(4)}
-                {this.renderCol(5)}
-                {this.renderCol(6)}
-                </div>
-                <div className="last">
-                <button className="restart" onClick = {() => this.handleReset()}>
-                    {'RESTART'}
-                </button>
+                
                 </div>
             </div>
         );
     }
 }
 
+/* called from get_state
+count the number of yellow, red and empty in the sequence of 4 passed
+and assign scores according to how close to win or lose
+computer winning should be positive scores
+player winning should be negative scores
+*/
+
 function calc(a, b, c, d) {
     let y = 0;
     let r = 0;
+    //Count for Each
 
-    if(a === 'Yellow') {
-        y++;
-    }
-    if(b === 'Yellow') {
-        y++;
-    }
-    if(c === 'Yellow') {
-        y++;
-    }
-    if(d === 'Yellow') {
-        y++;
-    }
-
-    if(a === 'Red') {
-        r++;
-    }
-    if(b === 'Red') {
-        r++;
-    }
-    if(c === 'Red') {
-        r++;
-    }
-    if(d === 'Red') {
-        r++;
-    }
+    /* these are very random values i have given based on intuition
+    and it seems to be working well */
+    /*Check these references for better understanding
+    https://medium.com/analytics-vidhya/artificial-intelligence-at-play-connect-four-minimax-algorithm-explained-3b5fc32e4a4f
+    https://towardsdatascience.com/creating-the-perfect-connect-four-ai-bot-c165115557b0
+    */
 
     if(y === 4 && r === 0) {
         return 5000;
@@ -289,21 +191,25 @@ function calc(a, b, c, d) {
         return 50;
     } 
     else if(y === 2 && r === 0) {
-        return 5;
+        return 4;
     }
     else if(y === 0 && r === 4) {
-        return -5000;
+        return -3000;
     }
     else if(y === 0 && r === 3) {
-        return -50;
+        return -100;
     }
     else if(y === 0 && r === 2) {
-        return -5;
+        return -10;
     }
     else {
         return 0;
     }
 }
+
+/* We check similar to gameover
+for every winnable sequence of 4 positions
+what they are filled with or how many are empty*/
 
 function get_state(tempBoard) {
     var total = 0;
@@ -312,29 +218,28 @@ function get_state(tempBoard) {
         for (let r = 0; r < 3; r++)
             total += calc(tempBoard[c][r], tempBoard[c][r+1], tempBoard[c][r+2], tempBoard[c][r+3]);
 
-    //HORIZONTAL
-    for (let r = 0; r < 6; r++)
-        for (let c = 0; c < 4; c++)
-            total += calc(tempBoard[c][r], tempBoard[c+1][r], tempBoard[c+2][r], tempBoard[c+3][r]);
-
-    //DIAGONAL
-    for (let r = 0; r < 3; r++)
-        for (let c = 0; c < 4; c++)
-            total += calc(tempBoard[c][r], tempBoard[c+1][r+1], tempBoard[c+2][r+2], tempBoard[c+3][r+3]);
-
-    //ANTIDIAGONAL
-    for (let r = 0; r < 3; r++)
-        for (let c = 3; c < 7; c++)
-            total += calc(tempBoard[c][r], tempBoard[c-1][r+1], tempBoard[c-2][r+2], tempBoard[c-3][r+3]);
-
-    return total;
+    //Do for other 3 directions and return total
 }
+
+/*
+Return true if all 4 positions have the same colour
+Else return false
+NOTE(possible bug): if all 4 have null then it should return false 
+since the game is not over, don't return true there
+*/
 
 function check(a, b, c, d) {
-    if((a != null) && (a === b) && (b ===c) && (c === d))
-        return true;
-    return false;
+
 }
+
+/*
+Function to check if the game is over
+check if there exists a winner
+take all possible combinations of 4 that can win
+and pass it to the check function to see if they are all the same
+I have checked for all vertical conditions
+In a similar manner check for the other 3 directions
+*/
 
 function gameOver(board) {
     //VERTICAL
@@ -344,39 +249,33 @@ function gameOver(board) {
                 return true;
     
     //HORIZONTAL
-    for (let r = 0; r < 6; r++)
-        for (let c = 0; c < 4; c++)
-            if (check(board[c][r], board[c+1][r], board[c+2][r], board[c+3][r]))
-                return true;
 
     //DIAGONAL
-    for (let r = 0; r < 3; r++)
-        for (let c = 0; c < 4; c++)
-            if (check(board[c][r], board[c+1][r+1], board[c+2][r+2], board[c+3][r+3]))
-                return true;
-
+    
     //ANTIDIAGONAL
-    for (let r = 0; r < 3; r++)
-        for (let c = 3; c < 7; c++)
-            if (check(board[c][r], board[c-1][r+1], board[c-2][r+2], board[c-3][r+3]))
-                return true;
 
     return null;
 }
+
+/* 
+Topmost Component Game
+Call Board Component from game
+write one line to call board below the h1 header
+no props passed 
+*/
 
 class Game extends React.Component {
     render() {
         return (
             <div>
                 <h1>Connect4</h1>
-                <Board />
             </div>
         );
     }
 }
 
 // ========================================
-
+// Entry Point of Code, Dont change anything
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
